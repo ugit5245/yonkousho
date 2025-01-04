4c767b8c4fff58099dc70404757f076dce89bce2
ugit5245 <umex.orque2@gmail.com>
2025-01-05 02:14:02 +0900

    add:設問マイグレーション前

---
+++ database/migrations/2025_01_04_223710_create_questions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('book_page');
+            $table->text('question_number');
+            $table->text('question_title');
+            $table->text('question_content');
+            $table->text('recommended_approach');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+};
