d8f709d5738518b382e58e877b25c2235dcc0f3e
ugit5245 <umex.orque2@gmail.com>
2025-01-11 02:16:33 +0900

    add:設問showで設問情報表示実験

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -3,4 +3,10 @@
 @section('title','questions')
 @section('content')
 <p>show単独設問</p>
+<p>{{$question->question_number}}</p>
+<p>{{$question->question_title}}</p>
+<p>{{$question->question_content}}</p>
+<p>{{$question->recommended_approach}}</p>
+
+
 @endsection
