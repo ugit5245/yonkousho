923379994270f5a14bbaf3eef47b6b49f2418922
ugit5245 <umex.orque2@gmail.com>
2025-01-13 02:59:30 +0900

    add:設問show表形式に

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -3,12 +3,26 @@
 @section('title','questions')
 @section('content')
 <p>show単独設問</p>
-<p>{{$question->question_number}}</p>
-<p>{{$question->question_title}}</p>
-<p>{{$question->question_content}}</p>
-<p>{{$question->recommended_approach}}</p>
 
+<div>
+<table>
+<td>{{$question->question_number}}</td>
+<td>{{$question->question_title}}</td>
+<td>{{$question->question_content}}</td>
+<td>{{$question->recommended_approach}}</td>
+</table>
+</div>
+
+<p>知識カードリスト</p>
+
+<table>
 @foreach ($knowledge_cards as $knowledge_card)
-<p>{{ $knowledge_card->card_title }},{{ $knowledge_card->card_content }}</p>
+<tr>
+<td>{{ $knowledge_card->card_title }}</td>
+<td>{{ $knowledge_card->card_content }}</td>
+
+</tr>
 @endforeach
+</table>
+
 @endsection
