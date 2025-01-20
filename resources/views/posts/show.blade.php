a46793c6bfb91460d2a5341b41e735259c00f5f9
ugit5245 <umex.orque2@gmail.com>
2025-01-21 02:13:44 +0900

    change:見出し、画面サイズなど調整

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,16 +3,17 @@
 @section('title','posts')
 
 @section('content')
-<div>記事名</div>
-<div>{{ $post->post_title }}</div>
-<div>書籍名</div>
+<h1>{{ $post->post_title }} 攻略</h1>
+<h2>書籍紹介</h2>
 <div>{{ $book->book_name }}</div>
 
+<h2>設問攻略</h2>
 @foreach ($Xquestions as $Xquestion)
-<div>{{ $Xquestion->question_title }}</div>
+<H3>{{ $Xquestion->question_title }}</H3>
+<div>{{ $Xquestion->question_content }}</div>
 
 @if ($Xquestion->question_has_cards->isEmpty())
-<div>知識なし</div>
+<div></div>
 @else
 
 @foreach ($Xquestion->question_has_cards as $knowledge_card )
