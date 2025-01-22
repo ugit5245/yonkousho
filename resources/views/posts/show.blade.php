62b17804f81dcf571a79bb59397574b4c725b69d
ugit5245 <umex.orque2@gmail.com>
2025-01-23 02:55:40 +0900

    add:ページ内リンク付き目次追加

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,12 +4,28 @@
 
 @section('content')
 <h1>{{ $post->post_title }} 攻略</h1>
+
+<div>entry-status
+<ul>
+<li style="display:inline">更新日</li>|<li style="display:inline">公開日</li>
+</ul>
+</div>
+
+<div>関連リンク</div>
+
+<div>目次</div>
+@foreach ($Xquestions as $Xquestion)
+<div><a href="#{{$Xquestion->question_title}}">{{ $Xquestion->question_title }}</a></div>
+@endforeach
+
 <h2>書籍紹介</h2>
 <div>{{ $book->book_name }}</div>
 
+
 <h2>設問攻略</h2>
 @foreach ($Xquestions as $Xquestion)
-<H3>{{ $Xquestion->question_title }}</H3>
+
+<H3><span id="{{$Xquestion->question_title}}">{{ $Xquestion->question_title }}</span></H3>
 <div>{{ $Xquestion->question_content }}</div>
 
 @if ($Xquestion->question_has_cards->isEmpty())
