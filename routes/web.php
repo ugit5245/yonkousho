b8115df73a8728718a1adfcc002f3e3431e0f075
ugit5245 <umex.orque2@gmail.com>
2025-01-24 05:39:46 +0900

    feat:Breeze導入インストール、上書きされたルートと親ビューを回復

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,44 @@
 <?php
 
-use App\Http\Controllers\CardhasQuestionController;
-use Illuminate\Support\Facades\Route;
+// Breeze導入で追加
+use App\Http\Controllers\ProfileController;
+
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\KnowledgeCardController;
 use App\Http\Controllers\QuestionHasCardController;
+use App\Http\Controllers\CardhasQuestionController;
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
+
+// Breeze導入で追加
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+// Breeze導入ここまで
+
 
 Route::get('question/{question}', [CardhasQuestionController::class, 'show'])->name('question.show');
 
@@ -29,3 +52,4 @@ Route::delete('question/{x}/{y}', [CardhasQuestionController::class, 'detach'])-
 Route::resource('posts',PostController::class);
 
 Route::resource('questions', QuestionController::class);
+
