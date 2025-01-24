2c307601336ea9816039b6927b56d63ea0cce01d
ugit5245 <umex.orque2@gmail.com>
2025-01-24 12:20:05 +0900

    fix:設問カードひもづけforeach内のgetElementbyIdを修正

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -15,38 +15,41 @@
 
 <p>知識カードリスト</p>
 
-<form method="POST">
-  @csrf
 
-  <table>
-    @foreach ($knowledge_cards as $knowledge_card)
-    <tr>
-      <td>{{ $knowledge_card->id }}</td>
-      <td>{{ $knowledge_card->card_title }}</td>
-      <td>{{ $knowledge_card->card_content }}</td>
-      <td>
 
-       
+
+<!-- foreach内のgetElementByIdとその相手先の組合せはユニークに -->
+
+<table>
+  @foreach ($knowledge_cards as $knowledge_card)
+  <tr>
+    <td>{{ $knowledge_card->id }}</td>
+    <td>{{ $knowledge_card->card_title }}</td>
+    <td>{{ $knowledge_card->card_content }}</td>
+    <td>
+      <form method="POST">
+        @csrf
+
         @if($knowledge_card->card_has_questions()->where('question_id', $question->id)->exists())
-        <a href="{{ route('QwithC.detach', ['x' => $question->id, 'y' => $knowledge_card->id]) }}" onclick="event.preventDefault(); document.getElementById('QwithC.detach-form').submit();">ひもづけ解除</a>
-        
+        <a href="{{ route('QwithC.detach', ['x' => $question->id, 'y' => $knowledge_card->id]) }}" onclick="event.preventDefault(); document.getElementById('QwithC.detach-form-{{$knowledge_card->id}}').submit();">ひもづけ解除</a>
+
         @else
-        <a href="{{ route('QwithC.attach', ['x' => $question->id, 'y' => $knowledge_card->id]) }}" onclick="event.preventDefault(); document.getElementById('QwithC.store-form').submit();">ひもづけ</a>
+        <a href="{{ route('QwithC.attach', ['x' => $question->id, 'y' => $knowledge_card->id]) }}" onclick="event.preventDefault(); document.getElementById('QwithC.store-form-{{$knowledge_card->id}}').submit();">ひもづけ</a>
         @endif
-</form>
+      </form>
 
-<form id="QwithC.detach-form" action="{{ route('QwithC.attach', ['x' => $question->id, 'y' => $knowledge_card->id]) }}" method="POST">
-  @csrf
-  @method('DELETE')
-</form>
+      <form id="QwithC.detach-form-{{$knowledge_card->id}}" action="{{ route('QwithC.attach', ['x' => $question->id, 'y' => $knowledge_card->id]) }}" method="POST">
+        @csrf
+        @method('DELETE')
+      </form>
 
-<form id="QwithC.store-form" action="{{ route('QwithC.attach', ['x' => $question->id, 'y' => $knowledge_card->id]) }}" method="POST">
-  @csrf
-</form>
+      <form id="QwithC.store-form-{{$knowledge_card->id}}" action="{{ route('QwithC.attach', ['x' => $question->id, 'y' => $knowledge_card->id]) }}" method="POST">
+        @csrf
+      </form>
 
-</td>
-</tr>
-@endforeach
+    </td>
+  </tr>
+  @endforeach
 </table>
 
 @endsection
