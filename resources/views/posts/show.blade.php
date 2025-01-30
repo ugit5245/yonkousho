f7e73d326baf6a2aac58f61f6e5f8ec155cc1708
ugit5245 <umex.orque2@gmail.com>
2025-01-31 03:05:18 +0900

    change:メイン上部にCSS適用

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,21 +6,29 @@
 <h1>{{ $post->post_title }} 攻略</h1>
 
 <div>
-<ul id="entry-date">
-<li style="display:inline">更新日：{{$post->updated_at->format('Y年m月d日')}}</li>
-<li style="display:inline">公開日：{{$post->created_at->format('Y年m月d日')}}</li>
-</ul>
+  <ul id="entry-date">
+    <li style="display:inline">更新日：{{$post->updated_at->format('Y年m月d日')}}</li>
+    <li style="display:inline">公開日：{{$post->created_at->format('Y年m月d日')}}</li>
+  </ul>
 </div>
 
-<div>関連リンク</div>
-@foreach ($Xposts as $a)
-<div><a href="{{ route('posts.show',$a->id) }}">{{ $a->book_page }}</a></div>
-@endforeach
+<div>
+  <span>関連リンク</span>
+  <div id="relative-link">
+    @foreach ($Xposts as $a)
+    <a href="{{ route('posts.show',$a->id) }}">{{ $a->book_page }}</a>
+    @endforeach
+  </div>
+</div>
 
-<div>目次</div>
-@foreach ($Xquestions as $Xquestion)
-<div><a href="#{{$Xquestion->question_title}}">{{ $Xquestion->question_title }}</a></div>
-@endforeach
+<div id="table-of-contents">
+  <div>目次</div>
+  <ul>
+    @foreach ($Xquestions as $Xquestion)
+    <li><a href="#{{$Xquestion->question_title}}">{{ $Xquestion->question_title }}</a></li>
+    @endforeach
+  </ul>
+</div>
 
 <h2>書籍紹介</h2>
 <div>{{ $book->book_name }}</div>
@@ -29,7 +37,7 @@
 <h2>設問攻略</h2>
 @foreach ($Xquestions as $Xquestion)
 
-<H3><span id="{{$Xquestion->question_title}}">{{ $Xquestion->question_title }}</span></H3>
+<H3><span id="{{$Xquestion->question_number}}">{{ $Xquestion->question_number }}</span>：<span id="{{$Xquestion->question_title}}">{{ $Xquestion->question_title }}</span></H3>
 <div>{!! nl2br(e($Xquestion->question_content)) !!}</div>
 
 @if ($Xquestion->question_has_cards->isEmpty())
@@ -37,12 +45,13 @@
 @else
 
 <div id="cards-area">
-@foreach ($Xquestion->question_has_cards as $knowledge_card )
-<div id="one-card">
-<div>{{ $knowledge_card->card_title }}</div>
-<div>{{ $knowledge_card->card_content }}</div>
-</div>
-@endforeach
+  @foreach ($Xquestion->question_has_cards as $knowledge_card )
+  <div id="one-card">
+    <div>{{ $knowledge_card->card_title }}</div>
+    <div>{{ $knowledge_card->card_content }}</div>
+    <div>{{ $knowledge_card->card_content }}</div>
+  </div>
+  @endforeach
 </div>
 
 @endif
