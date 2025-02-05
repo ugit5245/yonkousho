6a023f15f9c4ee05193d5f25b7fb10ab961329ad
ugit5245 <umex.orque2@gmail.com>
2025-02-05 16:00:38 +0900

    change:bootstrap有効下の独自CSSをimportantフラグで対応

--- resources/views/mycomponents/header.blade.php
+++ resources/views/mycomponents/header.blade.php
@@ -1,24 +1,24 @@
 <div id="header-area">
 
   <div id="common-bar">
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mr-5 mt-2">
+    <div>
+      <ul>
         @guest
-        <li class="nav-item mr-5">
+        <li>
           <a class="nav-link" href="{{ route('register') }}">登録</a>
         </li>
-        <li class="nav-item mr-5">
-          <a class="nav-link" href="{{ route('login') }}">ログイン</a>
+        <li>
+          <a href="{{ route('login') }}">ログイン</a>
         </li>
         <hr>
-        <li class="nav-item mr-5">
+        <li>
           <a class="nav-link" href="{{ route('login') }}"><i class="far fa-heart"></i></a>
         </li>
         <li class="nav-item mr-5">
           <a class="nav-link" href="{{ route('login') }}"><i class="fas fa-shopping-cart"></i></a>
         </li>
         @else
-        <li class="nav-item mr-5">
+        <li>
           <span>{{ Auth::user()->name }}</span>
           <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
             ログアウト
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,15 +5,15 @@
 @section('content')
 <h1>{{ $post->post_title }} 攻略</h1>
 
-<div>
-  <ul id="entry-date">
-    <li style="display:inline">更新日：{{$post->updated_at->format('Y年m月d日')}}</li>
-    <li style="display:inline">公開日：{{$post->created_at->format('Y年m月d日')}}</li>
-  </ul>
+
+<div id="entry-date">
+  <span>更新日：{{$post->updated_at->format('Y年m月d日')}}</span>
+  <span>公開日：{{$post->created_at->format('Y年m月d日')}}</span>
 </div>
 
-<div>
-  <span>関連リンク</span>
+
+<div id="relative-link-area" class="area">
+  <span>ページ</span>
   <div id="relative-link">
     @foreach ($Xposts as $a)
     <a href="{{ route('posts.show',$a->id) }}">{{ $a->book_page }}</a>
@@ -21,40 +21,48 @@
   </div>
 </div>
 
-<div id="table-of-contents">
+
+<div id="table-of-contents" class="area">
   <div>目次</div>
   <ul>
     @foreach ($Xquestions as $Xquestion)
-    <li><a href="#{{$Xquestion->question_title}}">{{ $Xquestion->question_title }}</a></li>
+    <li><a href="#{{ $Xquestion->question_number }}">{{ $Xquestion->question_title }}</a></li>
     @endforeach
   </ul>
 </div>
 
+
 <h2>書籍紹介</h2>
-<div>{{ $book->book_name }}</div>
-<div id="book-img"><img src="{{ asset($book->image) }}"></div>
+<div class="area">
+  <h3>{{ $book->book_name }}</h3>
+  <div id="book-img"><img src="{{ asset($book->image) }}"></div>
+</div>
+
 
 <h2>設問攻略</h2>
 @foreach ($Xquestions as $Xquestion)
 
-<H3><span id="{{$Xquestion->question_number}}">{{ $Xquestion->question_number }}</span>：<span id="{{$Xquestion->question_title}}">{{ $Xquestion->question_title }}</span></H3>
-<div>{!! nl2br(e($Xquestion->question_content)) !!}</div>
+<section>
+  <H3><span id="{{$Xquestion->question_number}}">{{ $Xquestion->question_number }}</span>：<span id="{{$Xquestion->question_title}}">{{ $Xquestion->question_title }}</span></H3>
+  <div>{!! nl2br(e($Xquestion->question_content)) !!}</div>
 
-@if ($Xquestion->question_has_cards->isEmpty())
-<div></div>
-@else
+  @if ($Xquestion->question_has_cards->isEmpty())
+  <div></div>
+  @else
 
-<div id="cards-area">
-  @foreach ($Xquestion->question_has_cards as $knowledge_card )
-  <div id="one-card">
-    <div>{{ $knowledge_card->card_title }}</div>
-    <div>{{ $knowledge_card->card_content }}</div>
-    <div>{{ $knowledge_card->card_content }}</div>
+  <div id="cards-area">
+    @foreach ($Xquestion->question_has_cards as $knowledge_card )
+    <section id="one-card">
+      <h4>{{ $knowledge_card->card_title }}</h4>
+      <div>{{ $knowledge_card->card_content }}</div>
+      <div>{{ $knowledge_card->source_category_code }}</div>
+    </section>
+    @endforeach
   </div>
-  @endforeach
-</div>
 
-@endif
+  @endif
+</section>
 @endforeach
+
 
 @endsection
